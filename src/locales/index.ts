eb053e5907e04d1d2c13d5273879baeb209b32f0
selbibegiyeva1 <selbibegiyeva1@gmail.com>
2026-01-29 13:35:21 +0500

    locales index commit

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -5,7 +5,10 @@ import { tm } from './tm';
 export { ru } from './ru';
 export { tm } from './tm';
 
-export type Translations = typeof ru;
+export type RuTranslations = typeof ru;
+export type TmTranslations = typeof tm;
+// Union of all locale shapes so both ru and tm are allowed
+export type Translations = RuTranslations | TmTranslations;
 
 const translations: Record<LanguageCode, Translations> = {
   ru,
